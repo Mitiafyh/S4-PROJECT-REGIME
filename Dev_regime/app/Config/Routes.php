edeaa92aef9253da104f577e25431581409c41e0
Mitiafyh <mitiafiorenantsoa@gmail.com>
2026-05-07 10:36:44 +0300

    ty

--- Dev_regime/app/Config/Routes.php
+++ Dev_regime/app/Config/Routes.php
@@ -6,3 +6,25 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+
+// Mitia
+$routes->get('/', 'LoginController::login');
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Noah
