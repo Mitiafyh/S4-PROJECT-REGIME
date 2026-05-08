88d03741877af0e61c06954295ae6cf448c51aa5
Mitiafyh <mitiafiorenantsoa@gmail.com>
2026-05-08 16:13:19 +0300

    Form

--- Dev_regime/app/Views/regimes/ajoutRegime.php
+++ Dev_regime/app/Views/regimes/ajoutRegime.php
@@ -9,19 +9,23 @@
     <h1>Ajouter un régime</h1>
     <form action="/insertRegime" method="post">
         <label for="Pourcentage_viande">Pourcentage de viande :</label>
-        <input type="number" id="Pourcentage_viande" name="Pourcentage_viande" required>
+        <input type="number" id="Pourcentage_viande" name="Pourcentage_viande" required><br>
 
         <label for="Pourcentage_poisson">Pourcentage de poisson :</label>
-        <input type="number" id="Pourcentage_poisson" name="Pourcentage_poisson" required>
+        <input type="number" id="Pourcentage_poisson" name="Pourcentage_poisson" required><br>
 
         <label for="Pourcentage_volaille">Pourcentage de volaille :</label>
-        <input type="number" id="Pourcentage_volaille" name="Pourcentage_volaille" required>
+        <input type="number" id="Pourcentage_volaille" name="Pourcentage_volaille" required><br>
 
         <label for="constatation">Constatation :</label>
-        <textarea id="constatation" name="constatation" required></textarea>
+        <textarea id="constatation" name="constatation" required></textarea><br>
 
         <label for="prixParSemaine">Prix par semaine :</label>
         <input type="number" id="prixParSemaine" name="prixParSemaine" step="0.01" required>
+
+        <label for="image">Image du régime :</label>
+        <input type="file" id="image" name="image"><br>
+        
 
         <button type="submit">Ajouter</button>
     </form>
